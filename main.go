5a349762daf4a684b9f3d7558bdbe992c1510d1b
murillo.ferreira <murillo.ferreira@bemobi.com.br>
2020-10-01 16:16:11 -0300

    fix port

--- main.go
+++ main.go
@@ -17,13 +17,13 @@ func upServer() {
 	port := os.Getenv("PORT")
 
 	if len(port) == 0 {
-		port = ":5000"
+		port = "5000"
 	}
 
 	buildRoutes()
 	fmt.Printf("Starting server at port %s\n", port)
 
-	if err := http.ListenAndServe(port, nil); err != nil {
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
