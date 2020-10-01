886b32c7e1bbe64abc8510669e5208e5954178b6
murillo.ferreira <murillo.ferreira@bemobi.com.br>
2020-10-01 15:49:58 -0300

    config port by env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"todo-go/todo"
 )
 
@@ -13,7 +14,11 @@ func main() {
 }
 
 func upServer() {
-	port := ":5000"
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = ":5000"
+	}
 
 	buildRoutes()
 	fmt.Printf("Starting server at port %s\n", port)
